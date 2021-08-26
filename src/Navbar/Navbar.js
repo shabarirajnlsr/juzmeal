0f8da730d49f088cc4b568590fe56694a6734d22
shabarirajnlsr <80581794+shabarirajnlsr@users.noreply.github.com>
2021-08-26 10:08:45 +0530

    Add files via upload

---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Navbar.css";
+
+class Navbar extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showdata: false,
+    };
+  }
+
+  render() {
+    return (
+      <div className="navbar">
+        <div className="leftnavbar">
+          <img src="JUZMEAL-Logo-Alone-Red.png" alt="nofile" />
+        </div>
+        <div className="rightnavbar">
+          <p>
+            <b>Home</b>
+          </p>
+          <p>
+            <b>About-Us</b>
+          </p>
+          <ul className="subnavlistone">
+            <b>Subscription</b>
+            <li>BreakFast</li>
+            <li>Lunch</li>
+            <li>Dinner</li>
+          </ul>
+          <p>
+            <b>Support</b>
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+export default Navbar;
