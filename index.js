0689e6bb95ce00252d1525a6a6bb457dd00163b9
shabarirajnlsr <80581794+shabarirajnlsr@users.noreply.github.com>
2021-08-26 10:13:40 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import App from './App'
-import 'semantic-ui-css/semantic.min.css'
-
-ReactDom.render(
-  <App/>,
-  document.getElementById("root")
-)
